2f5e15de7805e95fbd7576b6292df182196f1d2a
sumin <sue4869@naver.com>
2022-03-03 19:00:44 +0900

    이름 수정

---
+++ src/main/java/hello/bookListForPractice/RunBookList.java
@@ -0,0 +1,14 @@
+package hello.bookListForPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunBookList {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(RunBookList.class, args);
+	}
+
+}
